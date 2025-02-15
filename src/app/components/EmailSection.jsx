db4f7e1ec295cafbaecfe2dc119934239fc55118
Dev-Shannn <Nishantpandeyy07@gmail.com>
2025-02-15 22:45:26 +0530

    EmailSection.jsx

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -12,14 +12,16 @@ const ContactSection = () => {
       className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
     >
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
-      
+
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">Let&apos;s Connect</h5>
-        <p className="text-[#ADB7BE] mb-4 max-w-md leading-loose tracking-wide">
-          I&apos;m currently looking for new opportunities, and my inbox is always open.  
-          Whether you have a question or just want to say hi, I&apos;ll try my best to get back to you!
+        <p className="text-[#ADB7BE] mb-4 max-w-md text-lg leading-8">
+          I&apos;m currently looking for new opportunities,  
+          and my inbox is always open. <br />
+          Whether you have a question or just want to say hi, <br />
+          I&apos;ll try my best to get back to you!
         </p>
-        
+
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/Dev-Shannn">
             <Image src={GithubIcon} alt="Github Icon" />
