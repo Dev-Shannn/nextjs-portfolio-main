da6d28ae385b99545762f1b0c17e4fa3ed421a45
Dev-Shannn <Nishantpandeyy07@gmail.com>
2025-02-15 22:39:38 +0530

    EmailSection.jsx

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -17,7 +17,7 @@ const ContactSection = () => {
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           I&apos;m currently looking for new opportunities, and my inbox is always open.  
           Whether you have a question or just want to say hi,  
-          I&apos;ll try my best to get back to you!
+          I&apos;ll try my best to get back to you
         </p>
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/Dev-Shannn">
